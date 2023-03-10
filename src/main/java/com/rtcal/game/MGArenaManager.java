cc93d17ea1c28217574af76dd2ccafe5346c77de
rtcal <c.r.turasz@gmail.com>
2023-03-10 23:36:34 +0000

    Refactored for more meaningful names in MGArenaManager

--- src/main/java/com/rtcal/game/MGArenaManager.java
+++ src/main/java/com/rtcal/game/MGArenaManager.java
@@ -17,9 +17,9 @@ public final class MGArenaManager {
 
     private static volatile MGArenaManager instance;
 
-    private final Map<String, MGArena> arenas = new ConcurrentHashMap<>();
-    private final Map<String, Set<String>> arenasByType = new ConcurrentHashMap<>();
-    private final Map<String, Set<String>> arenasByMap = new ConcurrentHashMap<>();
+    private final Map<String, MGArena> registeredArenas = new ConcurrentHashMap<>();
+    private final Map<String, Set<String>> registeredArenasByTypeMapping = new ConcurrentHashMap<>();
+    private final Map<String, Set<String>> registeredArenasByMapNameMapping = new ConcurrentHashMap<>();
 
 
     private MGArenaManager() {
@@ -38,35 +38,35 @@ public final class MGArenaManager {
     }
 
     public boolean isRegisteredArena(@NotNull String name) {
-        return arenas.containsKey(name) && arenas.get(name) != null;
+        return registeredArenas.containsKey(name) && registeredArenas.get(name) != null;
     }
 
     public void registerArena(@NotNull MGArena arena) throws MGDuplicateException {
         if (isRegisteredArena(arena.getName())) throw new MGDuplicateException("MGArena with name '" + arena.getName() + "' already exists");
-        arenas.put(arena.getName(), arena);
-        arenasByType.computeIfAbsent(arena.getType(), k -> ConcurrentHashMap.newKeySet()).add(arena.getName());
-        arenasByMap.computeIfAbsent(arena.getMap().getName(), k -> ConcurrentHashMap.newKeySet()).add(arena.getName());
+        registeredArenas.put(arena.getName(), arena);
+        registeredArenasByTypeMapping.computeIfAbsent(arena.getType(), k -> ConcurrentHashMap.newKeySet()).add(arena.getName());
+        registeredArenasByMapNameMapping.computeIfAbsent(arena.getMap().getName(), k -> ConcurrentHashMap.newKeySet()).add(arena.getName());
     }
 
     // TODO: Ensure arena instance isn't running
     public void unregisterArena(@NotNull String name) throws NullPointerException {
         if (!isRegisteredArena(name)) throw new NullPointerException("MGArena '" + name + "' is not a registered arena");
 
-        arenasByType.computeIfPresent(arenas.get(name).getType(), (type, arenasOfType) -> {
+        registeredArenasByTypeMapping.computeIfPresent(registeredArenas.get(name).getType(), (type, arenasOfType) -> {
             arenasOfType.remove(name);
             return arenasOfType;
         });
 
-        arenasByMap.computeIfPresent(arenas.get(name).getMap().getName(), (map, arenasOfType) -> {
+        registeredArenasByMapNameMapping.computeIfPresent(registeredArenas.get(name).getMap().getName(), (map, arenasOfType) -> {
             arenasOfType.remove(name);
             return arenasOfType;
         });
 
-        arenas.remove(name);
+        registeredArenas.remove(name);
     }
 
-    public Map<String, MGArena> getArenas() {
-        return arenas;
+    public Map<String, MGArena> getRegisteredArenas() {
+        return registeredArenas;
     }
 
 }
